0389cb4a2fac6f17dd2eb55055436cc3dce56e30
MICROSOFT <d.vane.huancacruz@gmail.com>
2024-10-06 18:00:26 -0500

    Pom arreglado

--- Examen Tres_En_raya/src/main/java/pe/edu/upeu/Tres_raya/ApplicationTER.java
+++ Examen Tres_En_raya/src/main/java/pe/edu/upeu/Tres_raya/ApplicationTER.java
@@ -1,7 +1,21 @@
 package pe.edu.upeu.Tres_raya;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import pe.edu.upeu.Tres_raya.TERcontroller.juego_Controller;
+
 public class ApplicationTER {
     public static void main(String[] args) {
+        SpringApplication.run(TresRayaApplication.class, args);
         TresRayaApplication.main(args);
+    }
+
+    @Bean
+    public CommandLineRunner run(ApplicationContext context) { return args -> {
+        juego_Controller mx = context.getBean(juego_Controller.class);
+        mx.initialize();
+    };
     }
 }
